f01ff8c4edefca30f4b57ab9b9c716c7bd4480bb
trojan <saftab016@gmai.com>
2026-06-11 15:14:07 +0530

    fix: update navigation and layout paths to use base URL & resolve vite version issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://qrguy.com',
+  site: 'https://aftab016.github.io/qr-guy.com/',
+  base: '/qr-guy.com/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
